d8e15772a5089b2f907506cff4aa953a34e21f7d
Nikita <43261529+dexnick4501@users.noreply.github.com>
2022-05-21 18:03:55 +0500

    make api CRUD for Products

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return AnonymousResourceCollection
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        $products = Product::all();
+        return ProductResource::collection($products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return ProductResource
+     */
+    public function store(Request $request): ProductResource
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required',
+            'price' => 'required',
+        ]);
+        $product = Product::create($request->all());
+        return new ProductResource($product);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return ProductResource | Response
+     */
+    public function show($id): Response|ProductResource
+    {
+        try {
+            $product = Product::findOrFail($id);
+            return new ProductResource($product);
+        } catch (\Exception $exception) {
+            return response()->json(['message'=>'No have product with id:' . $id, 'code' => Response::HTTP_NOT_FOUND],Response::HTTP_NOT_FOUND);
+        }
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param int $id
+     * @param Request $request
+     * @return ProductResource | Response
+     */
+    public function update(Request $request, int $id): Response|ProductResource
+    {
+        try {
+            $product = Product::findOrFail($id);
+            $product->update($request->all());
+            return new ProductResource($product);
+        } catch (\Exception $exception) {
+            return response()->json(['message'=>'No have product with id:' . $id, 'code' => Response::HTTP_NOT_FOUND],Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return JsonResponse
+     */
+    public function destroy($id): JsonResponse
+    {
+        try {
+            $product = Product::findOrFail($id);
+            $product->delete();
+            return response()->json([
+                'message' => 'Success deleted',
+                'code' => Response::HTTP_OK,
+            ],Response::HTTP_OK);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'message' => 'No have product with id:' . $id,
+                'code' => Response::HTTP_NOT_FOUND,
+            ],Response::HTTP_NOT_FOUND);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+Route::apiResource('products',ProductController::class);
+
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
