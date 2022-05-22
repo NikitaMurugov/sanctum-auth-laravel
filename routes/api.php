f4cac74f8032d8d5455dccc3563db112efad4776
Nikita <43261529+dexnick4501@users.noreply.github.com>
2022-05-22 15:54:16 +0500

    restructure api.php

--- routes/api.php
+++ routes/api.php
@@ -21,19 +21,13 @@ use Symfony\Component\HttpFoundation\Response;
 
 
 
-Route::apiResource('products',ProductController::class)->only('index', 'show');
 Route::post('/register',[AuthController::class, 'register']);
 Route::post('/login',[AuthController::class, 'login']);
 
-//Route::get('/products',[ProductController::class, 'index']);
-//Route::get('/products/{product}',[ProductController::class, 'show']);
+Route::apiResource('products',ProductController::class)->only('index', 'show');
 Route::get('/products/search/{name}',[ProductController::class, 'search']);
 
 Route::group(['middleware'=> ['auth:sanctum']], function () {
     Route::apiResource('products',ProductController::class)->only('store', 'update', 'destroy');
-//    Route::post('/',[ProductController::class, 'store']);
-//    Route::match(array('PUT', 'PATCH'),'{product}',[ProductController::class, 'update']);
-//    Route::delete('{product}',[ProductController::class, 'destroy']);
-
     Route::post('/logout',[AuthController::class, 'logout']);
 });
